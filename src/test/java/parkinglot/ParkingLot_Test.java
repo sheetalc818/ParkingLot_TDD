ff07eb7d772c484193b2bd51fc9cbb45986155d1
sheetalc818 <sheetalc818@gmail.com>
2020-03-21 12:43:18 +0530

    Added Test to Check whether Vehicle is Unparked or not

--- src/test/java/parkinglot/ParkingLot_Test.java
+++ src/test/java/parkinglot/ParkingLot_Test.java
@@ -1,13 +1,22 @@
 package parkinglot;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLot_Test {
+
     @Test
     public void givenVehicle_AfterParked_ShouldReturnTrue() {
         ParkingSystem parkingSystem = new ParkingSystem();
         boolean isPark = parkingSystem.park(new Object());
         Assert.assertTrue(isPark);
+    }
+
+    @Test
+    public void givenVehicle_AfterUnParked_ShouldReturnTrue() {
+        ParkingSystem parkingSystem = new ParkingSystem();
+        boolean isUnPark = parkingSystem.unPark(new Object());
+        Assert.assertTrue(isUnPark);
     }
 }
