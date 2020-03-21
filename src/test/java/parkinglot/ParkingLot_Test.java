00d24d7acd50db39f1ae58bf78910c0c67e2d6b7
sheetalc818 <sheetalc818@gmail.com>
2020-03-21 13:25:20 +0530

    Added Negative Testcases for Parking and Unparking the vehicle

--- src/test/java/parkinglot/ParkingLot_Test.java
+++ src/test/java/parkinglot/ParkingLot_Test.java
@@ -7,23 +7,37 @@ import org.junit.Test;
 public class ParkingLot_Test {
 
     ParkingSystem parkingSystem = null;
+    Object vehicle = null;
 
     @Before
     public void setUp(){
         parkingSystem = new ParkingSystem();
+        vehicle = new Object();
     }
 
     @Test
     public void givenVehicle_AfterParked_ShouldReturnTrue() {
-        boolean isParked = parkingSystem.park(new Object());
+        boolean isParked = parkingSystem.park(vehicle);
         Assert.assertTrue(isParked);
     }
 
     @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingSystem.park(vehicle);
+        boolean isParked = parkingSystem.park(vehicle);
+        Assert.assertFalse(isParked);
+    }
+
+    @Test
     public void givenVehicle_AfterUnParked_ShouldReturnTrue() {
-        Object vehicle = new Object();
         parkingSystem.park(vehicle);
         boolean isUnPark = parkingSystem.unPark(vehicle);
         Assert.assertTrue(isUnPark);
+    }
+
+    @Test
+    public void noGivenVehicle_AfterUnParked_ShouldReturnFalse() {
+        boolean isUnPark = parkingSystem.unPark(null);
+        Assert.assertFalse(isUnPark);
     }
 }
