e582c0a04b9226fc8367123ad5b2196b2adccf64
sheetalc818 <sheetalc818@gmail.com>
2020-03-21 13:26:15 +0530

    Refactored park() and unParked() methods

--- src/main/java/parkinglot/ParkingSystem.java
+++ src/main/java/parkinglot/ParkingSystem.java
@@ -8,11 +8,16 @@ public class ParkingSystem {
     }
 
     public boolean park(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
         this.vehicle = vehicle;
         return true;
     }
 
     public boolean unPark(Object vehicle) {
+        if (vehicle == null){
+            return false;
+        }
         if (this.vehicle.equals(vehicle)) {
             this.vehicle = null;
             return true;
